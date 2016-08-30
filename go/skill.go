e9e5bd37de68d26d31e50fa85336b503f8bbe3ac
yku <kz2yz4@gmail.com>
2016-08-30 21:29:01 +0900

    add

---
+++ go/skill.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+	skill_time := flag.Int("t", 10, "skill charge time")
+	flag.Parse()
+
+	for reload := 1.0; reload <= 2.0; reload += 0.03 {
+		apply_time := float32(*skill_time) / float32(reload)
+		decrease := apply_time / float32(*skill_time) * 100.0
+		fmt.Printf("%3d%%\tcharge_time=%.3f[sec]\t%.3f%%\n", int32(reload*100.0)-100, apply_time, decrease)
+	}
+}
